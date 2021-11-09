81d952155872bcbaf18f7ee5980c2f17340fa4bd
swj <159632_@naver.com>
2021-11-10 03:24:25 +0900

    add:setting

--- app/src/main/java/com/dontsc/fcm/data/model/Motors.kt
+++ app/src/main/java/com/dontsc/fcm/data/model/Motors.kt
@@ -1,9 +1,17 @@
 package com.dontsc.fcm.data.model
 
 
+import com.google.firebase.database.IgnoreExtraProperties
 import com.google.gson.annotations.SerializedName
 
-
+/**
+ * @IgnoreExtraProperties : firebase 와 비교해서 알맞는 것만 적용시킨다.
+ * Properties that don't map to class fields are ignored when serializing to a class annotated with this annotation.
+ *
+ * @Exclude
+ * Marks a field as excluded from the Database.
+ */
+@IgnoreExtraProperties
 data class Motors(
   @SerializedName("m1")
   val m1: M1,
@@ -17,4 +25,6 @@ data class Motors(
   val m5: M5,
   @SerializedName("m6")
   val m6: M6
-)
+){
+
+}
--- app/src/main/java/com/dontsc/fcm/db/MyFirebaseDB.kt
+++ app/src/main/java/com/dontsc/fcm/db/MyFirebaseDB.kt
@@ -1,8 +1,7 @@
 package com.dontsc.fcm.db
 
 import android.util.Log
-import com.dontsc.fcm.data.model.M1
-import com.dontsc.fcm.data.model.Motors
+import com.dontsc.fcm.data.model.*
 import com.google.firebase.database.*
 import java.util.*
 import kotlin.collections.HashMap
@@ -23,16 +22,31 @@ object MyFirebaseDB {
       }
   }
 
+  /**
+   * addValueEventListener 경로의 전체 내용에 대한 변경 사항을 읽고 수신 대기한다. - 값이 바뀔 때 마다 onDataChange 호출
+   *
+   * addListenerForSingleValueEvent : 한 번 호출된 후 다시 호출 되지 않는다. - 1회성
+   * 한 번만 호출되고 즉시 삭제되는 콜백이 필요한 경우에 사용한다.
+   * 한 번 로드된 후 자주 변경되지 않거나 능동적으로 수신 대기할 필요가 없는 데이터에 유용하다.
+   *
+   * addChildEventListener : 데이터베이스의 특정한 노드에 대한 변경을 수신 대기하는데 유용하다.
+   * 목록을 다루는 앱은 단일 개체에 사용되는 값 이벤트보다는 하위 이벤트를 수신 대기해야한다.
+   * 하위 하목에 push() 메소드를 통해 새로 추가되거나 updateChildren() 메소드를 통해 업데이트 되는 경우가 그 예이다.
+   *
+   * DB 읽기 동작의 오버헤드를 줄이고 성능을 높이기 위해서 용도에 맞게 구현해야한다.
+   */
   fun readStock(id: String) {
     mDatabase.child("kiosk_id").child(id).child("motors")
-      .addValueEventListener(object : ValueEventListener {
+      .addListenerForSingleValueEvent(object : ValueEventListener {
         override fun onDataChange(snapshot: DataSnapshot) {
-          Log.e(TAG, "snapshot : $snapshot")
-          val post = snapshot.children.forEach {
-            Log.e(TAG, "foreach 데이터!: ${it.value}")
-          }
-
-//          Log.e(TAG, "onDataChange: $post")
+//          Motors(snapshot.children.map{it.value})
+//          Log.e(TAG, "onDataChange: ${post["m1"]}")
+//          Log.e(TAG, "snapshot : $snapshot")
+//          val post = snapshot.children.map { ds ->
+//            Log.e(TAG, "onDataChange: ${ds.key}")
+//            motors = Motors(ds.key, ds.child("stock").value)
+//            Log.e(TAG, "foreach 데이터!: ${ds.key}, ${ds.child("stock").value} ")
+//          }
         }
 
         override fun onCancelled(error: DatabaseError) {
