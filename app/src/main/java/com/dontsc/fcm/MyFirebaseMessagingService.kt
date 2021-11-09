2ad82b55394551dc44532b40cee34c364218c11e
wlwlsus <159632_@naver.com>
2021-11-09 10:27:54 +0900

    first commit

---
+++ app/src/main/java/com/dontsc/fcm/MyFirebaseMessagingService.kt
@@ -0,0 +1,38 @@
+package com.dontsc.fcm
+
+import android.util.Log
+import com.dontsc.view.MainActivity.Companion.liveTextView
+import com.google.android.gms.tasks.Task
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+
+open class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+  private val TAG = "MyFirebaseMsgService"
+
+  override fun onNewToken(p0: String) {
+    super.onNewToken(p0)
+
+    val refreshedToken: Task<String> = FirebaseMessaging.getInstance().token
+    Log.d(TAG, "Refreshed token: $refreshedToken")
+
+    sendRegistrationToServer(refreshedToken)
+  }
+
+  private fun sendRegistrationToServer(refreshedToken: Task<String>) {
+    Log.e(TAG, "sendRegistrationToServer: $refreshedToken")
+  }
+
+  override fun onMessageReceived(remoteMessage: RemoteMessage) {
+    Log.d(TAG, "From: " + remoteMessage.from)
+    if (remoteMessage.data.isNotEmpty()) {
+      Log.d(TAG, "Message data payload: " + remoteMessage.data)
+    }
+
+    remoteMessage.notification ?: return
+    Log.d(TAG, "Message Notification Body: " + remoteMessage.notification?.body)
+    liveTextView.postValue(remoteMessage.notification?.body)
+  }
+}
---
+++ app/src/main/java/com/dontsc/view/MainActivity.kt
@@ -0,0 +1,76 @@
+package com.dontsc.view
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.MutableLiveData
+import com.dontsc.fcm.R
+import com.google.firebase.messaging.FirebaseMessaging
+
+class MainActivity : AppCompatActivity() {
+
+  companion object {
+    var liveTextView: MutableLiveData<String> = MutableLiveData("Default Value")
+  }
+
+  private val TAG = "Main"
+
+  private var count = 0
+  private lateinit var textView: TextView
+  private lateinit var btn: Button
+  private lateinit var dbBtn: Button
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+
+    textView = findViewById(R.id.textView01)
+    btn = findViewById(R.id.button)
+    dbBtn = findViewById(R.id.db)
+
+    btn.text = count.toString()
+
+    btn.setOnClickListener {
+      count++
+      btn.text = count.toString()
+    }
+
+    liveTextView.observe(this, { code ->
+//      textView.text = code
+
+      val inst = code.split(" ")
+
+      when (inst[0]) {
+        "stop" -> {
+          if (count == inst[1].toInt()) {
+            Toast.makeText(this, "$count 번 키오스크 이벤트", Toast.LENGTH_SHORT).show()
+          }
+        }
+      }
+    })
+
+    FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+      if (!task.isSuccessful) {
+        Log.w(TAG, "Fetching FCM registration token failed", task.exception)
+        return@addOnCompleteListener
+      }
+
+      // Get new FCM registration token
+      val token = task.result
+      Log.e(TAG, "onCreate: $token")
+
+      // Log and toast
+      val msg = "MSG Token"
+      Log.d(TAG, msg)
+      Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+    }
+
+    dbBtn.setOnClickListener {
+      startActivity(Intent(this, DatabaseActivity::class.java))
+    }
+  }
+}
